3fe78696535457bbea9aa1aa98515c28517ef4b0
Yaron Y. Goland <yarong@microsoft.com>
2015-03-17 19:54:10 -0700

    Hacked to run v8 and includes commented out addTask code

--- jxcore-droid/assets/jxcore/app.js
+++ jxcore-droid/assets/jxcore/app.js
@@ -26,6 +26,38 @@ exports.concat = function(a, b, cb) {
   cb(a + b);
 };
 
+//jxcore.tasks.addTask(function() {
+//  // ugly patching
+//  var base_path = process.cwd();
+//  console.log("our base path is " + base_path);
+//  process.cwd = function () {
+//    //return base_path + "/jxcore_app/";
+//    return "/data/data/com.nubisa.jxcore/files";
+//  };
+//
+//  var express = require("express");
+//  var pouchDBApp = express();
+//  var PouchDB = require("pouchdb");
+//  var InMemPouchDB = PouchDB.defaults({db: require("memdown")});
+//
+//  var http = require('http');
+//  http.setMaxHeaderLength(0);
+//
+//  // Set up our test server
+//  pouchDBApp.use("/", require("express-pouchdb")(InMemPouchDB, {mode: "minimumForPouchDB"}));
+//  // Node tests by default will look at http://localhost:5984
+//  pouchDBApp.listen(5984);
+//
+//  require('pouchdb/tests/performance/index.js');
+//});
+
+
+// JXCore has a very useful security feature that limits the maximum size of HTTP request headers
+// But apparently the PouchDB tests require a larger size than the default maximum. So we disable
+// the check here.
+var http = require("http");
+http.setMaxHeaderLength(0);
+
 var express = require("express");
 var pouchDBApp = express();
 var PouchDB = require("pouchdb");
--- jxcore-droid/jni/Android.mk
+++ jxcore-droid/jni/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+# JXCORE_OUT_ANDROID := /home/yaron/Documents/depots/jxcore/out_android/android/bin/
 JXCORE_OUT_ANDROID := ../jxcore-binaries/
 
 include $(CLEAR_VARS)
@@ -55,12 +56,30 @@ LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libuv_arm.a
 endif
 include $(PREBUILT_STATIC_LIBRARY)
 
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libmozjs
+#ifeq ($(TARGET_ARCH),x86)
+#LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libmozjs_ia32.a
+#else
+#LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libmozjs_arm.a
+#endif
+#include $(PREBUILT_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := libmozjs
+LOCAL_MODULE := libv8base
 ifeq ($(TARGET_ARCH),x86)
-LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libmozjs_ia32.a
+LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libv8_base_ia32.a
 else
-LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libmozjs_arm.a
+LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libv8_base_arm.a
+endif
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libv8nosnapshot
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libv8_nosnapshot_ia32.a
+else
+LOCAL_SRC_FILES := $(JXCORE_OUT_ANDROID)libv8_nosnapshot_arm.a
 endif
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -85,5 +104,6 @@ LOCAL_LDLIBS := -llog \
                 -ldl \
                 -landroid
 LOCAL_STATIC_LIBRARIES := cares openssl
-LOCAL_WHOLE_STATIC_LIBRARIES := chrome_zlib http_parser sqlite3 libuv libmozjs jxcore_native
+# LOCAL_WHOLE_STATIC_LIBRARIES := chrome_zlib http_parser sqlite3 libuv libmozjs jxcore_native
+LOCAL_WHOLE_STATIC_LIBRARIES := chrome_zlib http_parser sqlite3 libuv libv8base libv8nosnapshot jxcore_native
 include $(BUILD_SHARED_LIBRARY)
